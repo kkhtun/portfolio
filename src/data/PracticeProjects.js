3364a7e89e1dfe696f7416b823fca9066ea6534f
kkhtun <khaingkhanthtun@gmail.com>
2022-08-18 18:17:20 +0630

    reorder practice projects

--- src/data/PracticeProjects.js
+++ src/data/PracticeProjects.js
@@ -8,13 +8,28 @@ import BadApple from "../images/bad-apple.jpg";
 const practiceProjects = [
     {
         id: 1,
-        title: "Book Store Project",
-        desc: "Built using Laravel, to includes admin book management, category management, user book download etc.",
-        image: Book,
-        github_url: "https://github.com/kkhtun/Laravel-Book-Store-Project",
+        title: "Photo Gallery",
+        desc: "A simple react tutorial photo gallery web app using Pixabay API to fetch image URLs.",
+        image: Pixabay,
+        github_url: "https://github.com/kkhtun/PhotoGallery-by-Pixabay-API",
     },
     {
         id: 2,
+        title: "Photo Uploading App",
+        desc: "A follow through make up of Photo uploading app using React and Firebase firestore/storage BaaS. ",
+        image: PhotoUploading,
+        github_url: "https://github.com/kkhtun/React-firegram",
+    },
+    {
+        id: 3,
+        title: "Bad Apple (Just for fun)",
+        desc: "Bad Apple on Browser Console but each frame is an API call",
+        image: BadApple,
+        github_url: "https://github.com/kkhtun/bad-apple-frames-api-ascii",
+        project_url: "https://www.youtube.com/watch?v=KyVkOUhbJyE",
+    },
+    {
+        id: 4,
         title: "WildFire Tracker",
         desc: "A simple React App to track wildfires using api.nasa.gov (NASA API for Natural Events) ",
         image: WildFire,
@@ -22,7 +37,7 @@ const practiceProjects = [
             "https://github.com/kkhtun/WildFire-Tracker-by-React-and-NASA-API",
     },
     {
-        id: 3,
+        id: 5,
         title: "Community Blog",
         desc: "Community Blog done as a part of PHP course, using pure PHP templating.",
         image: Blog,
@@ -30,26 +45,11 @@ const practiceProjects = [
             "https://github.com/kkhtun/php-course-community-blog-project",
     },
     {
-        id: 4,
-        title: "Photo Gallery",
-        desc: "A simple react tutorial photo gallery web app using Pixabay API to fetch image URLs.",
-        image: Pixabay,
-        github_url: "https://github.com/kkhtun/PhotoGallery-by-Pixabay-API",
-    },
-    {
-        id: 5,
-        title: "Photo Uploading App",
-        desc: "A follow through make up of Photo uploading app using React and Firebase firestore/storage BaaS. ",
-        image: PhotoUploading,
-        github_url: "https://github.com/kkhtun/React-firegram",
-    },
-    {
         id: 6,
-        title: "Bad Apple (Just for fun)",
-        desc: "Bad Apple on Browser Console but each frame is an API call",
-        image: BadApple,
-        github_url: "https://github.com/kkhtun/bad-apple-frames-api-ascii",
-        project_url: "https://www.youtube.com/watch?v=KyVkOUhbJyE",
+        title: "Book Store Project",
+        desc: "Built using Laravel, to includes admin book management, category management, user book download etc.",
+        image: Book,
+        github_url: "https://github.com/kkhtun/Laravel-Book-Store-Project",
     },
 ];
 
